d442dc613a6e3973a29c2e4aeb1f2480b7085fde
L102485 <hemaaltahas.hansda@network.lilly.com>
2026-02-19 13:53:12 +0530

    test:  Add generated Cucumber feature and steps files for Login test for KILO-1870

--- kilo-e2e-test-project/tests/default/KILO-1871/login-test-for-kilo-1870.steps.ts
+++ kilo-e2e-test-project/tests/default/KILO-1871/login-test-for-kilo-1870.steps.ts
@@ -3,19 +3,19 @@ import { Given, When, Then } from '@cucumber/cucumber';
 import { PlaywrightLoginPage } from '../pages/PlaywrightLoginPage';
 import { ICustomWorld } from '../support/world';
 
-let loginPage: PlaywrightLoginPage;
+let pageObject: PlaywrightLoginPage;
 
 Given('I am on the Playwright homepage', async function (this: ICustomWorld) {
-  loginPage = new PlaywrightLoginPage(this.page);
-  await loginPage.navigateToHomepage();
+  pageObject = new PlaywrightLoginPage(this.page);
+  await pageObject.navigateToHomepage();
 });
 
 When('I navigate to the login page and enter valid credentials', async function (this: ICustomWorld) {
-  await loginPage.navigateToLoginPage();
-  await loginPage.fillCredentials('test@example.com', 'password123');
-  await loginPage.submitLogin();
+  await pageObject.navigateToLoginPage();
+  await pageObject.fillLoginCredentials('test@example.com', 'securePassword123');
+  await pageObject.submitLogin();
 });
 
 Then('I should be successfully logged in', async function (this: ICustomWorld) {
-  await loginPage.expectSuccessfulLogin();
+  await pageObject.expectSuccessfulLogin();
 });
