4c9e26c4e1983efa9cc94ec484b9250582b4761d
L102485 <hemaaltahas.hansda@network.lilly.com>
2026-02-24 12:43:05 +0530

    test:  Add generated Cucumber feature and steps files for Test_csa_launchpad

---
+++ projects/kilo-e2e-test-project-testrxapp/tests/default/TESTRXAPP-785/testcsalaunchpad.steps.ts
@@ -0,0 +1,21 @@
+// Step Definitions
+import { Given, When, Then } from '@cucumber/cucumber';
+import { expect } from '@playwright/test';
+import { ICustomWorld } from '../support/world';
+import { PlaywrightHomePage } from '../pages/PlaywrightHomePage';
+
+let pageObject: PlaywrightHomePage;
+
+Given('I am on the Playwright homepage', async function (this: ICustomWorld) {
+  pageObject = new PlaywrightHomePage(this.page);
+  await pageObject.navigate();
+});
+
+When('I check for the Node.js button', async function (this: ICustomWorld) {
+  const isVisible = await pageObject.isNodeJsButtonVisible();
+  expect(isVisible).toBeTruthy();
+});
+
+Then('I should see the Node.js button displayed', async function (this: ICustomWorld) {
+  await pageObject.expectNodeJsButtonVisible();
+});
